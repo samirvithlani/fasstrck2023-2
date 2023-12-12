49094774c2a03f78c61470489fc7b603760330e6
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-12 12:56:12 +0530

    basics

---
+++ basics/basic1.py
@@ -0,0 +1,31 @@
+print("Hello world !!")
+
+#int no = 10 #error....
+#int, float ,str,bool ,NoneType
+
+#single line comment
+
+no = 10
+print(no)
+print("no = ",no)
+print("type of no = ",type(no))
+
+per = 90.5 #float
+print("per = ",per)
+print("type of per = ",type(per))
+
+name = "RAM"
+print("name = ",name)
+print("type of name = ",type(name))
+
+
+flag = False # 0 = false ,1 = true  True, False
+print("flag = ",flag)
+print("type of flag = ",type(flag))
+
+x = None
+print("x = ",x)
+print("type of x = ",type(x))
+
+    
+
---
+++ basics/basic2.py
@@ -0,0 +1,18 @@
+#get input from user...
+#input function....
+#input function always returns a string
+# "100"
+
+no = input("Enter a number: ")
+print("You entered: ", no)
+print(type(no))
+
+#type casting int
+no1 = int(input("Enter a number: "))
+print("You entered: ", no1)
+print(type(no1))
+
+
+per = float(input("Enter percentage: "))
+print("You entered: ", per)
+print(type(per))
